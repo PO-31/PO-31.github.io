9ffb0748f89a023ba8071e987d6f76b456d467ed
travis <travis@travis.org>
2018-04-09 09:22:12 +0000

    Update doc from Travis CI

---
+++ html/search/classes_5.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['group',['Group',['../classqtreports_1_1detail_1_1_group.html',1,'qtreports::detail']]]
+];
---
+++ html/search/classes_6.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['image',['Image',['../classqtreports_1_1detail_1_1_image.html',1,'qtreports::detail']]]
+];
