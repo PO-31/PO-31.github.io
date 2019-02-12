47f5bb234bfd4a0f24af1fbea11f0527e150c525
travis <travis@travis.org>
2019-02-12 15:41:49 +0000

    Update doc from Travis CI

---
+++ html/search/all_9.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['object',['Object',['../classqtreports_1_1detail_1_1_object.html',1,'qtreports::detail']]],
+  ['object',['Object',['../classqtreports_1_1detail_1_1_object.html#afcb591b3e6eb863351d731eacaf9d302',1,'qtreports::detail::Object']]],
+  ['open',['open',['../classqtreports_1_1_engine.html#a12cebdcb817b3c9a0c730b9439b2ff85',1,'qtreports::Engine']]],
+  ['optional',['Optional',['../classqtreports_1_1detail_1_1_parser_from_x_m_l.html#a0d7770fab9a27418149a44cb48f18a3faebb061953c0454b2c8ee7b0ac615ebcd',1,'qtreports::detail::ParserFromXML']]]
+];
---
+++ html/search/searchdata.js
@@ -0,0 +1,30 @@
+var indexSectionsWithContent =
+{
+  0: "abcdefgilopqrstw",
+  1: "bcdefgilopqrstw",
+  2: "acgioprs",
+  3: "aw",
+  4: "lor",
+  5: "q"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "functions",
+  3: "enums",
+  4: "enumvalues",
+  5: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "Указатель",
+  1: "Классы",
+  2: "Функции",
+  3: "Перечисления",
+  4: "Элементы перечислений",
+  5: "Страницы"
+};
+
