59e4adf31d0bc7314740b858580956b0798df0d1
travis <travis@travis.org>
2019-03-29 11:30:49 +0000

    Update doc from Travis CI

--- html/search/all_7.js
+++ html/search/all_7.js
@@ -4,8 +4,10 @@ var searchData=
   ['isbold',['isBold',['../classqtreports_1_1detail_1_1_style.html#a9f4c8c581c08ffd758fbd3e1b93dee07',1,'qtreports::detail::Style::isBold()'],['../classqtreports_1_1detail_1_1_widget.html#ae66db5f810d564e46f1cbb5e3643fa18',1,'qtreports::detail::Widget::isBold()']]],
   ['isdefault',['isDefault',['../classqtreports_1_1detail_1_1_style.html#ae3b307b5c92ded10d3bd63cced5d3a79',1,'qtreports::detail::Style']]],
   ['isitalic',['isItalic',['../classqtreports_1_1detail_1_1_style.html#a058eab1051810ffdb26d43fb08e523c9',1,'qtreports::detail::Style']]],
+  ['islayout',['isLayout',['../classqtreports_1_1detail_1_1_converter_to_q_widget.html#a76bb37f2855d12afc9056c75cdbc5e0b',1,'qtreports::detail::ConverterToQWidget']]],
   ['isopened',['isOpened',['../classqtreports_1_1_engine.html#aa3bb792af61b2661226814de9c3881ea',1,'qtreports::Engine']]],
   ['ispdfembedded',['isPDFEmbedded',['../classqtreports_1_1detail_1_1_style.html#a1f58498aef2437cc4689ab4408560cc1',1,'qtreports::detail::Style']]],
+  ['isreport',['isReport',['../classqtreports_1_1detail_1_1_converter_to_q_widget.html#a5e49976fe8fac12633ccda1ba928b9b1',1,'qtreports::detail::ConverterToQWidget']]],
   ['isstrikethrough',['isStrikeThrough',['../classqtreports_1_1detail_1_1_style.html#ac8cc338b439918b845de9f96023840e3',1,'qtreports::detail::Style']]],
   ['isunderline',['isUnderline',['../classqtreports_1_1detail_1_1_style.html#a879b9cf0745255ff5a45d45de39e6b3e',1,'qtreports::detail::Style']]]
 ];
